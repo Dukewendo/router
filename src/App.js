e12bff59b68b45c93284f175113c282e154fb0de
Dukewendo <dommct@yahoo.co.uk>
2023-04-25 13:38:57 +0100

    router-test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
-import Home from './pages/Home';
-import About from './pages/About';
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+//Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+//Layouts
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path ="/" element ={<RootLayout />}>
+      <Route index="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
-    <BrowserRouter>
-    <header>
-      <nav>
-        <h1>Jobs</h1>
-        <Link to="/">Home</Link>
-        <NavLink to="about">About</NavLink>
-      </nav>
-    </header>
-      <main>
-        <Routes>
-          <Route index ="/" element ={<Home />} />
-          <Route path ="about" element ={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+ 
+    <RouterProvider router={router} />
   );
 }
 
