a47d1f07f2b85d84b2e09f45181fc89201c62c24
Dukewendo <dommct@yahoo.co.uk>
2023-05-03 20:48:54 +0100

    react-forms

--- src/App.js
+++ src/App.js
@@ -15,49 +15,45 @@ import CareersLayout from "./layouts/Careerslayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Faq from "./pages/Help/Faq";
-import Contact from "./pages/Help/Contact";
+import Contact, { contactAction } from "./pages/Help/Contact";
 import NotFound from "./pages/NotFound";
 import Careers, { careersLoader } from "./pages/Career/Careers";
-import CareerDetails, { careerDetailsLoader } from "./pages/Career/CareerDetails";
+import CareerDetails, {
+  careerDetailsLoader,
+} from "./pages/Career/CareerDetails";
 import CareersError from "./pages/Career/CareersError";
-
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path ="/" element ={<RootLayout />}>
+    <Route path="/" element={<RootLayout />}>
       <Route index="/" element={<Home />} />
       <Route path="about" element={<About />} />
 
-      <Route path="help" element ={<HelpLayout />}>
-        <Route path="faq" element={<Faq />}/>
-        <Route path="contact" element={<Contact />}/>
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" element={<Faq />} />
+        <Route path="contact" element={<Contact />} action={contactAction}/>
       </Route>
 
-      <Route path="careers" element={<CareersLayout />} errorElement={<CareersError />}>
-        <Route 
-        index
-        element ={<Careers />} 
-        loader={careersLoader} 
-        />
+      <Route
+        path="careers"
+        element={<CareersLayout />}
+        errorElement={<CareersError />}
+      >
+        <Route index element={<Careers />} loader={careersLoader} />
         <Route
-        path=":id"
-        element={<CareerDetails />}
-        loader = {careerDetailsLoader}
-        >
-        </Route>
+          path=":id"
+          element={<CareerDetails />}
+          loader={careerDetailsLoader}
+        ></Route>
       </Route>
 
-      <Route path="*" element ={<NotFound />}
-      />
-      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Route>
   )
 );
 
 function App() {
-  return (
- 
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
